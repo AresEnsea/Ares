51a8dfc2f0e5b7016be605c485cf3bc936f9e555
Mowibox <ousmanethiongane17@gmail.com>
2023-08-28 18:01:14 +0200

    Ajout du dossier Activité de rentrée 2023

---
+++ Activité de rentrée 2023/Partie programmation/niveau2-tricolore.cpp
@@ -0,0 +1,30 @@
+#include "mbed.h"
+
+//Niveau 2 - Tricolore 
+//Mission : Changer de manière successive l’état des 3 LEDs
+
+DigitalOut redLed(PA_10);
+DigitalOut yellowLed(PB_5);
+DigitalOut greenLed(PA_8);
+
+int main()
+{
+    while (true) {
+        redLed = 1;
+        yellowLed = 0;
+        greenLed = 0;
+        ThisThread::sleep_for(1000ms);
+
+        redLed = 0;
+        yellowLed = 1;
+        greenLed = 0;
+        ThisThread::sleep_for(1000ms);
+
+        redLed = 0;
+        yellowLed = 0;
+        greenLed = 1;
+        ThisThread::sleep_for(1000ms);
+
+    }
+}
+
